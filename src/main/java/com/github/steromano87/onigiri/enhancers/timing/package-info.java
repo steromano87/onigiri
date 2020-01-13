be32da81efd70a572633c60af926fd81b71fdb71
Stefano Romanò <rumix87@gmail.com>
2020-01-14 00:19:27 +0100

    Added package-info files for all the modules

---
+++ src/main/java/com/github/steromano87/onigiri/enhancers/timing/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * {@link com.github.steromano87.onigiri.enhancers.Enhancer} implementation
+ * that records the duration of a method execution and prints it in the log.
+ */
+package com.github.steromano87.onigiri.enhancers.timing;
---
+++ src/main/java/com/github/steromano87/onigiri/handlers/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Package containing various adaptation layers for browsers, devices and so on.
+ */
+package com.github.steromano87.onigiri.handlers;
