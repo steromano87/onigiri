74ebf1e6f630a5c80959c0a48c2c85f2cd77b0e4
Stefano Romanò <rumix87@gmail.com>
2020-01-14 00:01:50 +0100

    Added Javadoc for the Settings class

--- src/main/java/com/github/steromano87/onigiri/Settings.java
+++ src/main/java/com/github/steromano87/onigiri/Settings.java
@@ -7,19 +7,96 @@ import org.apache.commons.configuration2.builder.fluent.Parameters;
 import org.apache.commons.configuration2.ex.ConfigurationException;
 import org.apache.commons.configuration2.io.ClasspathLocationStrategy;
 
+/**
+ * Utility class that holds the overall Onigiri settings.
+ * <p>
+ * This class serves two main purposes:
+ * <ul>
+ *     <li>holds a singleton instance of a {@link Configuration} object that stores
+ *     the Onigiri settings</li>
+ *     <li>hold the list of Onigiri property keys for an easier access throughout the code</li>
+ * </ul>
+ * <p>
+ * The {@link Configuration} item is built as a composite configuration:
+ * <ol>
+ *     <li>first of all the default values are loaded from the Onigiri factory settings</li>
+ *     <li>if a <code>onigiri.properties</code> file is found in the resources folder,
+ *     all the properties defined inside this file override the default values</li>
+ *     <li>if some property is set as a system property, this property has the highest priority and
+ *     overrides the previously defined value</li>
+ * </ol>
+ */
 public class Settings {
     // Sync properties
+    /**
+     * Whether the sync enhancer is enabled or not in the current project.
+     *
+     * @see com.github.steromano87.onigiri.enhancers.syncing.SyncingEnhancer
+     */
     public static final String SYNC_ENABLED = "onigiri.sync.enabled";
+
+    /**
+     * Whether the sync cache is enabled or not in the current project.
+     * <p>
+     * If the sync cache is enabled, the sync conditions for a page will be evaluated only once
+     * per page, speeding up the execution but with a higher risk of stale elements (the page may
+     * have changed in the meanwhile).
+     * <p>
+     * If the sync cache is disabled, the sync conditions will be always evaluated before executing a
+     * {@link com.github.steromano87.onigiri.enhancers.syncing.Synced} method. This will ensure the
+     * page is always loaded before performing an action, but the execution speed wil be lower due to
+     * the repeated evaluations.
+     */
     public static final String SYNC_CACHE_ENABLED = "onigiri.sync.cache.enabled";
+
+    /**
+     * The default timeout for sync.
+     */
     public static final String SYNC_WAIT_TIMEOUT = "onigiri.sync.wait.timeout";
+
+    /**
+     * The default polling interval between two consecutive page sync evaluations.
+     */
     public static final String SYNC_POLLING_INTERVAL = "onigiri.sync.polling.interval";
 
     // Stopwatch properties
+    /**
+     * Whether the stopwatch enhancer is enabled or not in the current project.
+     *
+     * @see com.github.steromano87.onigiri.enhancers.timing.StopwatchEnhancer
+     */
     public static final String STOPWATCH_ENABLED = "onigiri.stopwatch.enabled";
 
     // Element locators properties
+    /**
+     * Whether the element locator cache is enabled or not in the current project.
+     * <p>
+     * If the element locator cache is enabled, the element find query is performed only once per element.
+     * In the next calls to the element, the previously found {@link org.openqa.selenium.WebElement} is used.
+     * This behaviour greatly improves execution speed, but in case of a highly mutable page (i.e.
+     * with a strong presence of asynchronous Javascript scripts that dynamically change the page DOM)
+     * this can lead to element staleness.
+     * <p>
+     * If the element locator cache is disabled, every time an element is accessed its find query
+     * is recalculated. This ensures that the requested element is always "fresh", but the execution speed
+     * may be sensibly reduced.
+     */
     public static final String ELEMENT_LOCATORS_CACHE_ENABLED = "onigiri.element.locator.cache.enabled";
+
+    /**
+     * Default element locator timeout.
+     */
     public static final String ELEMENT_LOCATOR_TIMEOUT = "onigiri.element.locator.timeout";
+
+    /**
+     * Force the use of Onigiri's {@link com.github.steromano87.onigiri.ui.ExtendedElement}s instead
+     * of the default Selenium ones.
+     * <p>
+     * If this option is set to <code>true</code>, all the elements declared as plain
+     * {@link org.openqa.selenium.WebElement} will be rendered as
+     * {@link com.github.steromano87.onigiri.ui.ExtendedElement}. Elements already declared as
+     * {@link com.github.steromano87.onigiri.ui.ExtendedElement} will not be affected.
+     */
     public static final String ELEMENT_LOCATOR_FORCE_EXTENDED = "onigiri.element.locator.force.extended";
 
     // Page properties
