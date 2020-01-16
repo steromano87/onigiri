8050883fdfc4a476fb0d841c79b6d16452c34d38
Stefano Romanò <rumix87@gmail.com>
2020-01-17 00:16:11 +0100

    Added Draggable interface to ExtendedWebElement, added Javadoc

--- src/main/java/io/github/steromano87/onigiri/ui/mobile/ExtendedMobileElement.java
+++ src/main/java/io/github/steromano87/onigiri/ui/mobile/ExtendedMobileElement.java
@@ -3,8 +3,9 @@ package io.github.steromano87.onigiri.ui.mobile;
 import io.github.steromano87.onigiri.ui.AbstractExtendedElement;
 import io.github.steromano87.onigiri.ui.ExtendedElement;
 import io.appium.java_client.MobileElement;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.Coordinates;
+import org.openqa.selenium.interactions.touch.TouchActions;
 
 public class ExtendedMobileElement extends AbstractExtendedElement implements ExtendedElement {
     @Override
@@ -15,5 +16,23 @@ public class ExtendedMobileElement extends AbstractExtendedElement implements Ex
     @Override
     public Coordinates getCoordinates() {
         return ((MobileElement) this.getWrappedElement()).getCoordinates();
+    }
+
+    @Override
+    public void dragTo(WebElement target) throws NoSuchElementException {
+        TouchActions actions = new TouchActions(this.getWrappedDriver());
+        actions.longPress(this.getWrappedElement()).moveToElement(target).release().perform();
+    }
+
+    @Override
+    public void dragTo(By targetBy) throws NoSuchElementException {
+        this.dragTo((WebElement) this.getWrappedDriver().findElement(targetBy));
+    }
+
+    @Override
+    public void dragTo(Point targetPoint) {
+        TouchActions actions = new TouchActions(this.getWrappedDriver());
+        actions.longPress(this.getWrappedElement())
+                .move(targetPoint.getX(), targetPoint.getY()).release().perform();
     }
 }
--- src/main/java/io/github/steromano87/onigiri/ui/web/ExtendedWebElement.java
+++ src/main/java/io/github/steromano87/onigiri/ui/web/ExtendedWebElement.java
@@ -3,7 +3,8 @@ package io.github.steromano87.onigiri.ui.web;
 import io.github.steromano87.onigiri.handlers.JavascriptHandler;
 import io.github.steromano87.onigiri.ui.AbstractExtendedElement;
 import io.github.steromano87.onigiri.ui.ExtendedElement;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.interactions.Coordinates;
 import org.openqa.selenium.remote.RemoteWebElement;
 
@@ -21,5 +22,25 @@ public class ExtendedWebElement extends AbstractExtendedElement implements Exten
     @Override
     public void trigger(String event) {
         new JavascriptHandler(this.getWrappedDriver()).triggerEvent(this, event);
+    }
+
+    @Override
+    public void dragTo(WebElement target) throws NoSuchElementException {
+        Actions actions = new Actions(this.getWrappedDriver());
+        actions.dragAndDrop(this.getWrappedElement(), target);
+    }
+
+    @Override
+    public void dragTo(By targetBy) throws NoSuchElementException {
+        this.dragTo((WebElement) this.getWrappedDriver().findElement(targetBy));
+    }
+
+    @Override
+    public void dragTo(Point targetPoint) {
+        // TODO: check if the offset is correctly calculated
+        int xOffset = targetPoint.getX() - this.getWrappedElement().getLocation().getX();
+        int yOffset = this.getWrappedElement().getLocation().getY() - targetPoint.getY();
+        Actions actions = new Actions(this.getWrappedDriver());
+        actions.dragAndDropBy(this.getWrappedElement(), xOffset, yOffset);
     }
 }
