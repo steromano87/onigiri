0bf6f42dcf361583569f9a45c13797b491f853f2
Stefano Romanò <rumix87@gmail.com>
2020-01-17 00:16:49 +0100

    Added Javadoc to main page-related classes

--- src/main/java/io/github/steromano87/onigiri/ui/Draggable.java
+++ src/main/java/io/github/steromano87/onigiri/ui/Draggable.java
@@ -1,16 +1,32 @@
 package io.github.steromano87.onigiri.ui;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.Point;
 import org.openqa.selenium.WebElement;
 
 /**
- * Interface implemented by WebElements that can be dragged over another web element (or to a defined position)
+ * Interface implemented by {@link WebElement}s that can be dragged over another web element
+ * (or to a defined position).
  */
 public interface Draggable {
-    void dragTo(WebElement target);
+    /**
+     * Drags the current element on the given element.
+     * @param target the element to be dragged to
+     * @throws NoSuchElementException if the element is not found in the current DOM
+     */
+    void dragTo(WebElement target) throws NoSuchElementException;
 
-    void dragTo(By targetBy);
+    /**
+     * Drags the current element on the element identified by the given expression
+     * @param targetBy the find expression of the element to be dragged to
+     * @throws NoSuchElementException if the element is not found in the current DOM
+     */
+    void dragTo(By targetBy) throws NoSuchElementException;
 
+    /**
+     * Drags the current element to the specified point (expressed in the current viewport)
+     * @param targetPoint the point to be dragged to
+     */
     void dragTo(Point targetPoint);
 }
