e1d6478de87d2d8350cfe0f71d29703c868f5f7a
Stefano Romanò <rumix87@gmail.com>
2020-01-17 00:16:11 +0100

    Added Draggable interface to ExtendedWebElement, added Javadoc

--- src/main/java/io/github/steromano87/onigiri/ui/ExtendedElement.java
+++ src/main/java/io/github/steromano87/onigiri/ui/ExtendedElement.java
@@ -5,9 +5,20 @@ import org.openqa.selenium.interactions.Locatable;
 
 /**
  * The main interface for all the extended WebElements defined in Onigiri.
- *
- * This interface must be implemented by all the extended WebElements defined in Onigiri.
+ * <p>
+ * {@link ExtendedElement} are a superset of standard Selenium {@link WebElement} that enrich
+ * the base interface with additional methods and properties.
+ * <p>
+ * Since all {@link ExtendedElement} are defined as wrappers to a simple {@link WebElement},
+ * the only method defined in the base interface allow the setting of the wrapped element without
+ * the need to define a non-zero argument constructor.
  */
-public interface ExtendedElement extends WebElement, Locatable, SearchContext, TakesScreenshot, WrapsDriver, WrapsElement {
+public interface ExtendedElement extends WebElement, Draggable,
+        Locatable, SearchContext, TakesScreenshot, WrapsDriver, WrapsElement {
+    /**
+     * Sets the base element that is wrapped by the {@link ExtendedElement}
+     *
+     * @param wrappedElement the base element that will be wrapped
+     */
     void setWrappedElement(WebElement wrappedElement);
 }
