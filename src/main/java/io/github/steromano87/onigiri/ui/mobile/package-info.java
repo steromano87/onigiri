009ab4271774644454dcd2ad97ce01cf6d1328e4
Stefano Romanò <rumix87@gmail.com>
2020-01-30 22:14:39 +0100

    Removed mobile-related classes from versioning.
    These classes are always stashed, for future development.

--- src/main/java/io/github/steromano87/onigiri/ui/mobile/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Package containing all the native mobile implementations of the interfaces defined in the
- * {@link io.github.steromano87.onigiri.ui} package.
- */
-package io.github.steromano87.onigiri.ui.mobile;
