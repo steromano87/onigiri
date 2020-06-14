809405a6ba8230eeb6986ee17e5832fbbd6922f5
Stefano Romanò <rumix87@gmail.com>
2020-06-14 15:13:12 +0200

    Added Javadoc for enhancer-related classes

--- src/main/java/io/github/steromano87/onigiri/enhancers/AfterMethodPriority.java
+++ src/main/java/io/github/steromano87/onigiri/enhancers/AfterMethodPriority.java
@@ -5,6 +5,13 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Specifies the priority (i.e. the order of application) of an {@link AfterMethodEnhancer}.
+ *
+ * The order of application is calculated using the value passed using this annotation, in direct order.
+ *
+ * @see AfterMethodEnhancer
+ */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface AfterMethodPriority {
